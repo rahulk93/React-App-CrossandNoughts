b185ef99cccb50d79cdcb38cf8b5357e06795e5a
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 11:37:57 +1300

    Updates

---
+++ precache-manifest.d52c17ab0e8dab5f2473cf82480ea458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8942c58962c237b7a4b59878e0b7b51f",
+    "url": "/React-App-CrossandNoughts/index.html"
+  },
+  {
+    "revision": "6dab177756db153dd028",
+    "url": "/React-App-CrossandNoughts/static/css/main.6fac61e4.chunk.css"
+  },
+  {
+    "revision": "4361e00dfe6d7eb7f1bc",
+    "url": "/React-App-CrossandNoughts/static/js/2.b2d2b9ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-CrossandNoughts/static/js/2.b2d2b9ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dab177756db153dd028",
+    "url": "/React-App-CrossandNoughts/static/js/main.b34537f5.chunk.js"
+  },
+  {
+    "revision": "73a2bcd58b08e5b2c697",
+    "url": "/React-App-CrossandNoughts/static/js/runtime-main.9797c88c.js"
+  }
+]);
